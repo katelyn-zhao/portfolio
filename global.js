286e136bcb996bf527f6d8644bea73bc23b1992e
Katelyn Zhao <k5zhao@ucsd.edu>
2025-01-24 00:27:46 -0800

    update

--- global.js
+++ global.js
@@ -15,25 +15,22 @@ let pages = [
 let nav = document.createElement('nav');
 document.body.prepend(nav);
 
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
+
 for (let p of pages) {
     let url = p.url;
     let title = p.title;
+    if (!ARE_WE_HOME && !url.startsWith('http')) {
+        url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+      }
     let a = document.createElement('a');
     a.href = url;
     a.textContent = title;
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+      }
+    if (a.host !== location.host) {
+        a.target = "_blank";
+      }
     nav.append(a);
-  }
-
-const ARE_WE_HOME = document.documentElement.classList.contains('home');
-
-if (!ARE_WE_HOME && !url.startsWith('http')) {
-    url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
-  }
-
-if (a.host === location.host && a.pathname === location.pathname) {
-    a.classList.add('current');
-  }
-
-if (a.host !== location.host) {
-    a.target = "_blank";
   }

63aadee0c1fc52ecd9acd704167284c745be6837
Katelyn Zhao <k5zhao@ucsd.edu>
2025-01-23 23:53:09 -0800

    update

--- global.js
+++ global.js
@@ -22,3 +22,7 @@ for (let p of pages) {
   }
 
 const ARE_WE_HOME = document.documentElement.classList.contains('home');
+
+if (!ARE_WE_HOME && !url.startsWith('http')) {
+    url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+  }

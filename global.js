f2cdace6dd20b918ab7ddba3e62a8a3d58397a88
Katelyn Zhao <k5zhao@ucsd.edu>
2025-01-30 17:29:50 -0800

    update

--- global.js
+++ global.js
@@ -19,10 +19,7 @@ const ARE_WE_HOME = document.documentElement.classList.contains('home');
 
 for (let p of pages) {
     let url = p.url;
-    let title = p.title;
-    // if (!ARE_WE_HOME && !url.startsWith('http')) {
-    //     url = '../' + url;
-    //  }    
+    let title = p.title;    
     let a = document.createElement('a');
     a.href = url;
     a.textContent = title;
@@ -71,3 +68,33 @@ select.addEventListener('input', (event) => {
   localStorage.setItem('colorScheme', selectedScheme);
   updateColorScheme(selectedScheme);
 });
+
+export async function fetchJSON(url) {
+  try {
+      // Fetch the JSON file from the given URL
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Failed to fetch projects: ${response.statusText}`);
+    }
+    const data = await response.json();
+    return data; 
+
+
+  } catch (error) {
+      console.error('Error fetching or parsing JSON data:', error);
+  }
+}
+
+export function renderProjects(project, containerElement) {
+  // Your code will go here
+  containerElement.innerHTML = '';
+  project.forEach(project => {
+    const article = document.createElement('article');
+    article.innerHTML = `
+    <h3>${project.title}</h3>
+    <img src="${project.image}" alt="${project.title}">
+    <p>${project.description}</p>
+`;
+  containerElement.appendChild(article);
+  });
+}

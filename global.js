e745348630d3a1540e230fcc7e16eaa07aeaa551
Katelyn Zhao <k5zhao@ucsd.edu>
2025-02-14 01:01:22 -0800

    update

--- global.js
+++ global.js
@@ -4,42 +4,14 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-// let pages = [
-//     {url: '/', title: 'Home'},
-//     {url: 'projects/', title: 'Projects'},
-//     {url: 'cv-resume/', title: 'CV/Resume'},
-//     {url: 'contact/', title: 'Contact'},
-//     {url: 'https://github.com/katelyn-zhao', title: 'GitHub'}
-// ]
-
-// let nav = document.createElement('nav');
-// document.body.prepend(nav);
-
-// const ARE_WE_HOME = document.documentElement.classList.contains('home');
-
-// for (let p of pages) {
-//     let url = p.url;
-//     let title = p.title;
-//     url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
-//     let a = document.createElement('a');
-//     a.href = url;
-//     a.textContent = title;
-//     nav.append(a);
-//     if (a.host == location.host && a.pathname === location.pathname) {
-//         a.classList.add('current');
-//     }
-//     if (a.host !== location.host) {
-//         a.target = '_blank';
-//     }
-// }
-
 let pages = [
-    { url: '/portfolio/', title: 'Home' },
-    { url: '/portfolio/projects/', title: 'Projects' },
-    { url: '/portfolio/cv-resume/', title: 'CV/Resume'},
-    { url: '/portfolio/contact/', title:'Contact'},
-    { url: 'https://github.com/katelyn-zhao', title:'Github'}
-  ];
+    {url: '/', title: 'Home'},
+    {url: 'projects/', title: 'Projects'},
+    {url: 'cv-resume/', title: 'CV/Resume'},
+    {url: 'contact/', title: 'Contact'},
+    {url: 'meta/', title: 'Meta'},
+    {url: 'https://github.com/katelyn-zhao', title: 'GitHub'}
+]
 
 let nav = document.createElement('nav');
 document.body.prepend(nav);
@@ -49,20 +21,50 @@ const ARE_WE_HOME = document.documentElement.classList.contains('home');
 for (let p of pages) {
     let url = p.url;
     let title = p.title;
-    // if (!ARE_WE_HOME && !url.startsWith('http')) {
-    //     url = '../' + url;
-    //  }    
+    url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
     let a = document.createElement('a');
     a.href = url;
     a.textContent = title;
-    if (a.host === location.host && a.pathname === location.pathname) {
-        a.classList.add('current');
-      }
-    if (a.host !== location.host) {
-        a.target = "_blank";
-      }
     nav.append(a);
-  }
+    if (a.host == location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+    }
+    if (a.host !== location.host) {
+        a.target = '_blank';
+    }
+}
+
+// let pages = [
+//     { url: '/portfolio/', title: 'Home' },
+//     { url: '/portfolio/projects/', title: 'Projects' },
+//     { url: '/portfolio/cv-resume/', title: 'CV/Resume'},
+//     { url: '/portfolio/contact/', title:'Contact'},
+//     { url: '/portfolio/meta/', title:'Meta'},
+//     { url: 'https://github.com/katelyn-zhao', title:'Github'}
+//   ];
+
+// let nav = document.createElement('nav');
+// document.body.prepend(nav);
+
+// const ARE_WE_HOME = document.documentElement.classList.contains('home');
+
+// for (let p of pages) {
+//     let url = p.url;
+//     let title = p.title;
+//     // if (!ARE_WE_HOME && !url.startsWith('http')) {
+//     //     url = '../' + url;
+//     //  }    
+//     let a = document.createElement('a');
+//     a.href = url;
+//     a.textContent = title;
+//     if (a.host === location.host && a.pathname === location.pathname) {
+//         a.classList.add('current');
+//       }
+//     if (a.host !== location.host) {
+//         a.target = "_blank";
+//       }
+//     nav.append(a);
+//   }
 
 document.body.insertAdjacentHTML(
     'afterbegin',
